b8b4d165bad6e13085daa7d7bb03caa06020204a
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-07-17 22:21:43 -0400

    :construction: Add quick sort basic + unit tests

---
+++ src/main/java/edu/neu/coe/huskyBenchmark/sort/quick/Partitioner.java
@@ -0,0 +1,13 @@
+package edu.neu.coe.huskyBenchmark.sort.quick;
+
+import java.util.List;
+
+public interface Partitioner<X extends Comparable<X>> {
+    /**
+     * Method to partition the given partition into smaller partitions.
+     *
+     * @param partition the partition to divide up.
+     * @return an array of partitions, whose length depends on the sorting method being used.
+     */
+    List<Partition<X>> partition(Partition<X> partition);
+}
